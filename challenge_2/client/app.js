5f40614e6f26842cb82550ffe02ca27fc00a7a59
alexanderwiederin <alexander.wiederin@gmail.com>
2018-10-16 17:28:15 -0700

    added json to csv conversion functionality

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,69 @@
+var csvConverter = (content) => {
+	var obj = {};
+	var csvString = '';
+
+	var addAllCols = (content) => {
+
+		if(Array.isArray(content)) {
+		  content.forEach((element) => {
+		  	addAllCols(element);
+		  });
+		} else if(typeof content === 'object') {
+		  for(var key in content) {
+		  	if(Array.isArray(content[key])) {
+		      addAllCols(content[key]);
+		    }
+		    if(key !== 'children') {
+			    obj[key] = [];
+		    }
+	    }
+	  }
+
+  }
+
+  var addAllRows = (content) => {
+
+		for(var key in content) {
+
+			if(key !== 'children') {
+
+				obj[key].push(content[key]);
+			} else {
+				content[key].forEach((child) => {
+					addAllRows(child);
+				})
+			}
+		}
+  	
+  }
+
+  var printData = (object) => {
+  	var columnNames = Object.keys(object);
+  	var columnLength = object[columnNames[0]].length;
+  	var totalArray = [];
+ 
+
+  	csvString += columnNames.join(",") + '\n';
+
+  	for(var i = 0; i < columnLength; i++) {
+  		var salesPersonArray = [];
+  		for(var key in object) {
+  			salesPersonArray.push(object[key][i]);
+  		}
+  		csvString += salesPersonArray.join(",") + '\n';
+  	}
 
 
+  }
+
+
+  addAllCols(content);
+  addAllRows(content);
+  printData(obj);
+
+  return csvString;
+
+ 
+}
+
+module.exports.csvConverter = csvConverter;
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var path = require('path');
+var parseBody = require('body-parser');
+var methods = require('./client/app.js')
 
 var app = express();
 
 app.use(express.static(path.join(__dirname + '/client')));
-§
+
+app.use(parseBody.urlencoded({ extended: false }));
+
+app.post('/submit', (request, reponse) => {
+	var content = request.body.content;
+	console.log(request.body);
+	// csvConverter(content, (error, results) => {
+	// 	reponse.send(results);
+	// });
+	reponse.status(201).end();
+
+});
+
+
 app.listen(3000); 
+
+
 
