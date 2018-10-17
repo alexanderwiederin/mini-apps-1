92e06b0293dfcc9ecb42331438e1782ff9ad1a18
alexanderwiederin <alexander.wiederin@gmail.com>
2018-10-17 12:03:11 -0700

    [completed] dev environment

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,13 @@
+var express = require('express');
+var path = require('path');
 
+var app = express();
+
+console.log(path.join(__dirname + '/public'));
+
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.listen(3000, () => {
+	console.log('listening on port 3000');
+})
 
