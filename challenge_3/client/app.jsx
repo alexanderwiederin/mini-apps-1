3c6c6b6c2f673c481cf4d644b1a1dfa00e84aa3f
alexanderwiederin <alexander.wiederin@gmail.com>
2018-10-17 17:25:08 -0700

    all forms working

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -11,6 +11,7 @@ class App extends React.Component {
 
 		this.submitF1 = this.submitF1.bind(this);
 		this.submitF2 = this.submitF2.bind(this);
+		this.submitF3 = this.submitF3.bind(this);
 	}
 
 	submitF1() {
@@ -52,6 +53,25 @@ class App extends React.Component {
 
 	}
 
+	submitF3() {
+		var credit_card_num  = document.getElementById('creditCardNum').value;
+		var expiry_date = document.getElementById('expiryDate').value;
+		var billing_zip_code = document.getElementById('billingZipCode').value;
+
+		var formData = {
+			credit_card_num,
+			expiry_date,
+			billing_zip_code,
+		};
+
+		var oldFormData = this.state.formData;
+
+		var newFormData = Object.assign(formData, oldFormData);
+
+		this.setState({form: 4, formData: newFormData});
+
+	}
+
 	render() {
 		if(this.state.form === 0) {
 			return (
@@ -77,6 +97,32 @@ class App extends React.Component {
 					State: <input type="text" id="state"/>
 					Zip Code: <input type="text" id="zipCode"/>
 					<button onClick={this.submitF2}>next</button>
+				</div>
+			)
+		} else if(this.state.form === 3) {
+			return (
+				<div className="form">
+					Credit Card Number: <input type="text" id="creditCardNum"/>
+					Expiry Date: <input type="text" id="expiryDate"/>
+					Billing Zip Code: <input type="text" id="billingZipCode"/>
+					<button onClick={this.submitF3}>next</button>
+				</div>
+			)
+		} else if(this.state.form === 4) {
+			return (
+				<div className="finalCheck">
+					<div><span>Name: </span><span>{this.state.formData.name}</span></div>
+ 					<div><span>Email: </span><span>{this.state.formData.email}</span></div>
+					<div><span>Password: </span><span>{this.state.formData.password}</span></div>
+					<div><span>Address Line 1: </span><span>{this.state.formData.address_line_1}</span></div>
+					<div><span>Address Line 2: </span><span>{this.state.formData.address_line_2}</span></div>
+					<div><span>City: </span><span>{this.state.formData.city}</span></div>
+					<div><span>State: </span><span>{this.state.formData.state}</span></div>
+					<div><span>Zip Code: </span><span>{this.state.formData.zip_code}</span></div>
+					<div><span>Credit Card Number: </span><span>{this.state.formData.credit_card_num}</span></div>
+					<div><span>Expiry Date: </span><span>{this.state.formData.expiry_date}</span></div>
+					<div><span>Billing Zip Code: </span><span>{this.state.formData.billing_zip_code}</span></div>
+					<button>Purchase</button>
 				</div>
 			)
 		}
